2624e0798ba8e8da4539f71faade2eceb7e1b609
Esther Zinn <Bionicfemme@me.com>
2015-08-27 14:44:40 -0400

    got game pieces to appear with clicks

---
+++ tictactoe.js
@@ -0,0 +1,7 @@
+$(function() {
+$('.container > div').click( function() {
+  console.log('clicked!');
+        //$(this).css('background-image', 'url(/Images/X.jpg)');
+        $(this).text('x');
+    });
+})
