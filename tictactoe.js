a21c9aa4db08964aa93e78c50e4ff0ad6a87d6c5
Esther Zinn <Bionicfemme@me.com>
2015-08-28 09:50:23 -0400

    Finished computer AI, still need to finish checkWinner

--- tictactoe.js
+++ tictactoe.js
@@ -17,7 +17,7 @@ $(function() {
 					$(this).text('x');
 					c1 = "X";
 					count++;
-					checkWinner();
+
 					randomizer();
 				}
 			});
@@ -27,7 +27,7 @@ $(function() {
           $(this).text('x');
           c2 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -37,7 +37,7 @@ $(function() {
           $(this).text('x');
           c3 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -47,7 +47,7 @@ $(function() {
           $(this).text('x');
           c4 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -57,7 +57,7 @@ $(function() {
           $(this).text('x');
           c5 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -67,7 +67,7 @@ $(function() {
           $(this).text('x');
           c6 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -77,7 +77,7 @@ $(function() {
           $(this).text('x');
           c7 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -87,7 +87,7 @@ $(function() {
           $(this).text('x');
           c8 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
@@ -97,8 +97,80 @@ $(function() {
           $(this).text('x');
           c9 = "X";
           count++;
-          checkWinner();
+
           randomizer();
         }
       });
+
+      function randomizer() {
+				var Found = false;
+				while(Found != true) {
+					var Random = Math.floor(Math.random() * 9) + 1;
+
+          if(Random == 1 && c1 != "X" && c1 !="O") {
+            c1 = "O";
+            $('div#cell-1').text('o');
+            count++;
+
+            Found = true;
+          }
+					if(Random == 2 && c2 != "X" && c2 !="O") {
+						c2 = "O";
+						$('div#cell-2').text('o');
+						count++;
+
+						Found = true;
+					}
+					if(Random == 3 && c3 != "X" && c3 !="O") {
+						$('div#cell-3').text('o');
+						c3 = "O";
+						count++;
+
+						Found = true;
+					}
+					if(Random == 4 && c4 != "X" && c4 !="O") {
+						c4 = "O";
+						$('div#cell-4').text('o');
+						count++;
+
+						Found = true;
+					}
+					if(Random == 5 && c5 != "X" && c5 !="O") {
+						c5 = "O";
+						$('div#cell-5').text('o');
+						count++;
+
+						Found = true;
+					}
+					if(Random == 6 && c6 != "X" && c6 !="O") {
+						c6 = "O";
+						$('div#cell-6').text('o');
+						count++;
+
+						Found = true;
+					}
+					if(Random == 7 && c7 != "X" && c7 !="O") {
+						c7 = "O";
+						$('div#cell-7').text('o');
+						count++;
+
+						Found = true;
+					}
+					if(Random == 8 && c8 != "X" && c8 !="O") {
+						c8 = "O";
+						$('div#cell-8').text('o');
+						count++;
+
+						Found = true;
+					}
+					if(Random == 9 && c9 != "X" && c9 !="O") {
+						c9 = "O";
+						$('div#cell-9').text('o');
+						count++;
+
+						Found = true;
+					}
+				}
+				Found = false;
+			}
 })
